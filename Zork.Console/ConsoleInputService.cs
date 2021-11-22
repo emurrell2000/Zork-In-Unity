a34cbcd9ba4a869697df46e3a90119715f034a78
emurrell2000 <emurrell2000@gmail.com>
2021-11-22 18:03:16 -0500

    First Commit

---
+++ Zork.Console/ConsoleInputService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zork
+{
+    internal class ConsoleInputService : IInputService
+    {
+        public event EventHandler<string> InputRecieved;
+
+        public void ProcessInput()
+        {
+            string inputString = Console.ReadLine().Trim();
+            InputRecieved?.Invoke(this, inputString);
+        }
+    }
+}
